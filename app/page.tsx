bf82b05a6f1624e6a7ecf9a985e1f466154b5a9a
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-05-11 08:18:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,13 +31,13 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        SINEWAVE
+        TASK FORCE
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Security Researcher and Entrepreneur. Let's innovate!
+          Slogan goes here
         </h2>
       </div>
     </div>
