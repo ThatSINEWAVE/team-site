ff113e7998cf157b6be164aa747f11d463b23ee9
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-05-12 02:40:05 +0300

    Update particles.tsx

--- app/components/particles.tsx
+++ app/components/particles.tsx
@@ -124,7 +124,7 @@ export default function Particles({
 			context.current.translate(translateX, translateY);
 			context.current.beginPath();
 			context.current.arc(x, y, size, 0, 2 * Math.PI);
-			context.current.fillStyle = `rgba(173, 216, 230, ${alpha})`;
+			context.current.fillStyle = `rgba(255, 138, 0, ${alpha})`;
 			context.current.fill();
 			context.current.setTransform(dpr, 0, 0, dpr, 0, 0);
 
